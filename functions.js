0ed0c65edb903de43b4a4f75125a8e735029f8f0
muradfazliniyaz <107277134+muradfazliniyaz@users.noreply.github.com>
2022-12-06 07:50:04 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,10 +1,27 @@
-const getProductsOfNormalQuality = () => {};
+const getProductsOfNormalQuality = (pProductList) => {
+  return pProductList
+    .filter((product) => product.quality === "Normal")
+};
+console.log(getProductsOfNormalQuality);
 
-const getProductImageWithPngExtension = () => {};
+const getProductImageWithPngExtension = (pProductList) => {
+  return pProductList
+    .filter((product) => product.productImage === ".png")
+};
+console.log(getProductImageWithPngExtension);
 
-const getCalorieOfTheMostExpensiveProduct = () => {};
+const getCalorieOfTheMostExpensiveProduct = (pProductList) => {
+  return pProductList
+    .filter((product) => product.totalCalories)
+    .reduce((max, min) => max.price > min.price ? max : min)
+};
+console.log(getCalorieOfTheMostExpensiveProduct);
 
-const sortByExpirationDate = () => {};
+const sortByExpirationDate = (pProductList) => {
+  return pProductList
+    .sort((productA, productB) => productA.expireDate - productB.expireDate)
+};
+console.log(sortByExpirationDate);
 
 export {
   getProductsOfNormalQuality,
